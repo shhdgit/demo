6d0651b0147a59ce10ef839c10820961c6c8fb21
SUHAHA <jklopsdfw@yahoo.com>
2017-03-28 20:54:22 +0800

    Update jst1.js

--- ptt/js/task1/js/jst1.js
+++ ptt/js/task1/js/jst1.js
@@ -1,7 +1,7 @@
 window.onload = function () {
   var blocks = document.getElementById('wrap').children
   function changeColor (doms) {
-    // 还原颜色
+    // 如果有doms，则还原颜色（第一次执行的时候doms = []，见最后一行）
     var i, j
     var domsLen = doms.length
     if (domsLen) {
@@ -10,12 +10,13 @@ window.onload = function () {
       }
     }
     
-    // 获取3个随机dom元素，更新doms
+    // 获取随机dom改变颜色，更新doms
     for (j = 0; j < 3; j++) {
       doms[j] = blocks[getRandom(blocks.length)]
       doms[j].style.backgroundColor = '#' + (Math.random() * 0xffffff << 0)
     }
     
+    // 递归                                |-> 传递了本次保存的doms，对于下一次调用来说一开始的doms就是before
     setTimeout(function () { changeColor(doms) }, 1000)
   }
   function getRandom (base) {
